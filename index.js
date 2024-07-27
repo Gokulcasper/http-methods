642df272ad9564e82f32391e1496be27c8a312a3
Gokulcasper <ronitcasper215@gmail.com>
2024-07-27 20:32:28 +0530

    Commit PUT method

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+// let data={
+//     name:"Raj",
+//     gender:"Male",
+//     email:"raj@gmail.com",
+//     status:"InActive",
+// }
+// let Options={
+//     method:"POST",
+//     headers:{
+//         "Content-Type":"application/json",
+//         Accept:"application/json",
+//         Authorization:"Bearer cc9a788bd50afcfbbc87fd69e6a88228cb421b60f9b38a1f44a891802f59d8ef"
+//     },
+//     body: JSON.stringify(data)
+// };
+// fetch("https://gorest.co.in/public/v2/users", Options)
+// .then(function (response){
+//     return response.json()
+// })
+// .then(function (jsonData){
+//     console.log(jsonData);
+// })
+
+//--------PUT---------
 let data={
-    name:"Gokul",
+    name:"MuthuMani",
     gender:"Male",
-    email:"gokul@gmail.com",
-    status:"Active",
+    email:"muthumani@gmail.com",
+    status:"InActive",
 }
 let Options={
-    method:"POST",
+    method:"PUT",
     headers:{
         "Content-Type":"application/json",
         Accept:"application/json",
@@ -13,7 +37,7 @@ let Options={
     },
     body: JSON.stringify(data)
 };
-fetch("https://gorest.co.in/public/v2/users", Options)
+fetch("https://gorest.co.in/public/v2/users/7184055", Options)
 .then(function (response){
     return response.json()
 })
