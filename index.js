4af40a13e55f1e54d5b634e988ec25ea47efff15
Gokulcasper <ronitcasper215@gmail.com>
2024-07-27 20:12:47 +0530

    Commit Post Method

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+let data={
+    name:"Gokul",
+    gender:"Male",
+    email:"gokul@gmail.com",
+    status:"Active",
+}
 let Options={
-    method:"GET",
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json",
+        Accept:"application/json",
+        Authorization:"Bearer cc9a788bd50afcfbbc87fd69e6a88228cb421b60f9b38a1f44a891802f59d8ef"
+    },
+    body: JSON.stringify(data)
 };
 fetch("https://gorest.co.in/public/v2/users", Options)
 .then(function (response){
