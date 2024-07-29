ea8eb59dced92dee237c850d71754e41eb458333
Gokulcasper <ronitcasper215@gmail.com>
2024-07-29 07:39:33 +0530

    HTTP Methods Practiced

--- index.js
+++ index.js
@@ -48,18 +48,41 @@
 
 
 // ----------DETELE--------
-let Options={
+// let Options={
+//     method:"DELETE",
+//     headers:{
+//         "Content-Type":"application/json",
+//         Accept:"application/json",
+//         Authorization:"Bearer cc9a788bd50afcfbbc87fd69e6a88228cb421b60f9b38a1f44a891802f59d8ef"
+//     },
+//     body: JSON.stringify()
+// };
+// fetch("https://gorest.co.in/public/v2/users/7183829", Options)
+// .then(function (response){
+//     return response.json()
+// })
+// .then(function (jsonData){
+//     console.log(jsonData);
+// })
+
+let data={
+    name:"ThiruMala",
+    gender:"Male",
+    email:"Thirumala@gmail.com",
+    status:"Active",
+}
+let options={
     method:"DELETE",
     headers:{
         "Content-Type":"application/json",
         Accept:"application/json",
         Authorization:"Bearer cc9a788bd50afcfbbc87fd69e6a88228cb421b60f9b38a1f44a891802f59d8ef"
     },
-    body: JSON.stringify()
+body:JSON.stringify(data)
 };
-fetch("https://gorest.co.in/public/v2/users/7183829", Options)
+fetch("https://gorest.co.in/public/v2/users/7215243",options)
 .then(function (response){
-    return response.json()
+    return response.json();
 })
 .then(function (jsonData){
     console.log(jsonData);
